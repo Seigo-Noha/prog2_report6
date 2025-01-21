fd7839ed31c960b26f755040dc444edc53ade45c
Seigo-Noha <e245705@ie.u-ryukyu.ac.jp>
2025-01-21 23:15:30 +0900

    4th commit

--- app/src/test/java/jp/ac/uryukyu/ie/e245705/BlackjackTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e245705/BlackjackTest.java
@@ -8,4 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BlackjackTest {
 
+    @Test
+    public void testCalculateHand() {
+        Blackjack game = new Blackjack();
+        ArrayList<String> hand = new ArrayList<>();
+        hand.add("ハートA");
+        hand.add("ダイヤ10");
+        assertEquals(21, game.calculateHand(hand), "手札がAと10のとき合計は21");
+    }
 }
